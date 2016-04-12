f9a5303e7b06e1ed35e7bcb5cef4165ad027c4a3
Igor Serko <igor.serko@gmail.com>
2016-04-12 12:11:31 +0100

    added max_retries to see what happens

--- src/celery_test/tasks/__init__.py
+++ src/celery_test/tasks/__init__.py
@@ -3,7 +3,7 @@ from celery.contrib.batches import Batches
 from celery_test import app
 
 
-@app.task(base=Batches, flush_every=30, flush_interval=10)
+@app.task(base=Batches, flush_every=30, flush_interval=10, max_retries=5)
 def batch_exceptions(requests):
     print "Got {} requests".format(len(requests))
     for request in requests:
